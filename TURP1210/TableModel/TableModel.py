71bc7c8d1d99748da103a5ad177037846f1ad19a
jt-stancil <jts283@utulsa.edu>
2018-05-31 16:47:19 -0500

    J1939 Table and UDS tables
    Successfully coloring J1939 PGN table based on PGN and UDS table based on SID

--- TURP1210/TableModel/TableModel.py
+++ TURP1210/TableModel/TableModel.py
@@ -1,6 +1,6 @@
 
-from PyQt5.QtCore import Qt, QAbstractTableModel, QSortFilterProxyModel, QVariant
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, QAbstractTableModel, QSortFilterProxyModel, QVariant, QModelIndex
+from PyQt5.QtGui import QIcon, QBrush, QColor
 
 from collections import OrderedDict
 
@@ -14,6 +14,8 @@ class J1939TableModel(QAbstractTableModel):
 
     def setDataHeader(self, header):
         self.header = header
+        self.first = self.header[0]
+        print(self.first)
         self.header_len = len(self.header)
         
     def setDataDict(self, new_dict):
@@ -40,10 +42,64 @@ class J1939TableModel(QAbstractTableModel):
         if index.isValid() and role == Qt.DisplayRole:
             key = self.table_rows[index.row()]
             col_name = self.header[index.column()]
+            #print(key)
+            #print(col_name)
+            #print('hey')
+            #print(self.data_dict.values())
             return str(self.data_dict[key][col_name])
+        if role == Qt.BackgroundRole:
+            if self.first == 'PGN':
+                key = self.table_rows[index.row()]
+                pgn = key.split(',')[0][1:]
+                int_pgn = int(pgn)
+                if int_pgn == 65264:
+                    return QBrush(QColor(255,0,255))
+                elif int_pgn == 61440:
+                    return QBrush(QColor(255,0,125))
+                elif int_pgn == 65271:
+                    return QBrush(QColor(0,255,255))
+                elif int_pgn == 65270:
+                    return QBrush(QColor(125,255,0))  
+                elif int_pgn == 65269:
+                    return QBrush(QColor(125,0,255))
+                elif int_pgn == 65271:
+                    return QBrush(QColor(0,0,255))
+                elif int_pgn == 65263:
+                    return QBrush(QColor(0,255,125))
+                elif int_pgn == 65262:
+                    return QBrush(QColor(232,149,6))
+                elif int_pgn == 65259:
+                    return QBrush(QColor(255,250,0))
+                elif int_pgn == 65226:
+                    return QBrush(QColor(132,11,163))
+                elif int_pgn == 65266:
+                    return QBrush(QColor(255,119,119))
+                elif int_pgn == 65251:
+                    return QBrush(QColor(16,205,209))
+                elif int_pgn == 65217:
+                    return QBrush(QColor(183,228,255))
+                elif int_pgn == 65249:
+                    return QBrush(QColor(252,166,228))
+            elif self.first == "Acronym":
+                key = self.table_rows[index.row()]
+                col_name = self.header[index.column()]
+            elif self.first == "Line":
+                key = self.table_rows[index.row()]
+                col_name = self.header[index.column()]
+                print('hey')
+                stringSID = self.data_dict[key]['SID']
+                if stringSID == '2E':
+                    return QBrush(QColor(255,0,0))
+                elif stringSID == '22':
+                    return QBrush(QColor(0,255,0)) 
         else:
             return QVariant()
     
+    #def colorTable(self, index, role = Qt.BackgroundRole):
+        #if index.isValid():
+
+
+
     def flags(self, index):
             flags = super(J1939TableModel, self).flags(index)
             flags |= ~Qt.ItemIsEditable
